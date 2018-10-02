3d0f0c97f0e8fa1fcfce03713591c2e3b2154088
Daniel Trotter <flare84@gmail.com>
2018-10-02 09:57:57 -0600

    first commit

---
+++ config/initializers/devise.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+DeviseTokenAuth.setup do |config|
+  config.secret_key = '18110c92b0877065114cae0b985e6a97ddab5eeee6105a04284acf1f1bca644a4315a5b992120cc5b1b91a562ea9057189d626cbbd5a6f51ffc7f1e760330102'
+end
